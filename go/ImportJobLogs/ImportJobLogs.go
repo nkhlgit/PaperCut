fe861a716f250be6ca30522500ba2173d7f91b54
nik2017 <nikhil.eltrx@gmail.com>
2018-01-28 11:27:10 +1100

    Reduced Space & time

--- go/ImportJobLogs/ImportJobLogs.go
+++ go/ImportJobLogs/ImportJobLogs.go
@@ -1,8 +1,8 @@
 /*9-01-18@nikhil
-This will use PaperCut WebXML API to to connect PaperCut. Please check the IP and Token in follwing lines of script as per your PaperCut Server.
- v.Param[0].Arg1 = "token" 
-req, err := http.NewRequest("POST", "http://127.0.0.1:9191/rpc/api/xmlrpc", bytes.NewBuffer([]byte(output)))
+This will use PaperCut WebXML API to to connect PaperCut.
 This will  read the CSV and import Job logs from "c:\temp\Joblog_template.csv" to PaperCut Server
+User Has to Drfine as their environment> AuthToken, AppServer, Port variable, defined afer comment containg string ##UserDefine
+
 */
 
 package main
@@ -13,15 +13,64 @@ import (
 	"encoding/csv"
 	"encoding/xml"
 	"fmt"
+	//	"io/ioutil"
+	//"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
+// ##UserDefine Define all Auth_tokn , IP/host and port.
+var AuthToken = "token"
+var AppServer = "127.0.0.1"
+var Port = "9191"
+var ServerURL = "http://" + AppServer + ":" + Port + "/rpc/api/xmlrpc"
+
+// Initiating web Cleint connection
+var client = &http.Client{}
+
+//Define the XML stracture and initialized V as struct.
+type Param struct {
+	Arg1 string `xml:"value>string"`
+}
+type JobProcess struct {
+	XMLName xml.Name `xml:"methodCall"`
+	Method  string   `xml:"methodName"`
+	Param   [2]Param `xml:"params>param"`
+}
+
+var V JobProcess
+
+func importJob(jobString string) {
+	//Add the job String to V Struct
+	V.Param[1].Arg1 = jobString
+	//Marshell body of message
+	output, err := xml.MarshalIndent(V, "  ", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	// Intitiate the http request format.
+	req, err := http.NewRequest("POST", ServerURL, bytes.NewBuffer([]byte(output)))
+	req.Header.Add("Content-Type", "text/xml")
+	// Execute the http request.
+	resp, err := client.Do(req)
+	//, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	//	if err != nil {
+	//	log.Fatal(err)
+	//}
+	//println("What I got")
+	//log.Println(string(data))
+}
+
 func main() {
+	// Device JOb type and Auth_token
+	V.Method = "api.processJob"
+	V.Param[0].Arg1 = AuthToken
+
 	//CSV parse that contain jobs
-	f, _ := os.Open("C:\\tmp\\JobLogs_Template.csv")
+	f, _ := os.Open("C:\\pc\\go\\JobLogImport\\printlog_template.csv")
 	r := csv.NewReader(bufio.NewReader(f))
 	record, _ := r.ReadAll()
 	// Reader the header of CSV file
@@ -32,22 +81,6 @@ func main() {
 	var t time.Time
 	//define the time format of imput to read
 	format := "3:04:05 PM"
-
-	// Initiating web Cleint connection
-	client := &http.Client{}
-
-	//Define the XML stracture
-	type Param struct {
-		Arg1 string `xml:"value>string"`
-	}
-	type JobProcess struct {
-		XMLName xml.Name `xml:"methodCall"`
-		Method  string   `xml:"methodName"`
-		Param   [2]Param `xml:"params>param"`
-	}
-	// Assiging value to XML Variable
-	v := &JobProcess{Method: "api.processJob"}
-	v.Param[0].Arg1 = "token"
 
 	// Processing input rows
 	fmt.Print("Processing Row: ")
@@ -76,28 +109,8 @@ func main() {
 			}
 			j = j + 1
 		}
-		//fmt.Println(jobString)
-		// jobStream assigned 2nd string for jobprocess
-		v.Param[1].Arg1 = jobString
-		//Marshell body of message
-		output, err := xml.MarshalIndent(v, "  ", "    ")
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-		}
-		// Intitiate the http request format.
-		req, err := http.NewRequest("POST", "http://127.0.0.1:9191/rpc/api/xmlrpc", bytes.NewBuffer([]byte(output)))
-		req.Header.Add("Content-Type", "text/xml")
-		// Execute the http request.
-		client.Do(req)
-		/*		resp, err :=
-				data, err := ioutil.ReadAll(resp.Body)
-						resp.Body.Close()
-						if err != nil {
-							log.Fatal(err)
-						}
-						//println("Whayt I got")
-						//log.Println(string(data))
-		*/
+		importJob(jobString)
+
 	}
 
 }
